d15c49b7bcea73f622668cf2bc94f678eb91c968
Viri <Viri@DESKTOP-N9L019U>
2018-06-10 19:04:28 -0500

    VRG V-0.0.3-SNAPSHOT JPA CRUD

--- 01_RESTDemo/src/main/java/com/mitocode/controller/PersonaController.java
+++ 01_RESTDemo/src/main/java/com/mitocode/controller/PersonaController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,12 +47,52 @@ public class PersonaController {
 
 		return new ResponseEntity<List<Persona>>(personas, HttpStatus.OK);
 	}
+	
+	@GetMapping(value = "/listarId/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Persona> listarId(@PathVariable("id") Integer id) {
+		Persona persona = new Persona();
+		try {
+			persona = service.listarId(id);
+
+		} catch (Exception e) {
+			new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Persona>(persona, HttpStatus.OK);
+	}
 
 	@PostMapping(value="/registrar",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Integer> registrar(@RequestBody Persona per) {
 		int resultado = 0;
 		try {
 			service.registrar(per);
+			resultado = 1;
+		} catch (Exception e) {
+			new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Integer>(resultado, HttpStatus.OK);
+	}
+	
+	@PutMapping(value="/actualizar",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> actualizar(@RequestBody Persona per) {
+		int resultado = 0;
+		try {
+			service.modificar(per);
+			resultado = 1;
+		} catch (Exception e) {
+			new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Integer>(resultado, HttpStatus.OK);
+	}
+	
+	
+	@DeleteMapping(value="/eliminar/{id}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> eliminar(@PathVariable Integer id) {
+		int resultado = 0;
+		try {
+			service.eliminar(id);
 			resultado = 1;
 		} catch (Exception e) {
 			new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
